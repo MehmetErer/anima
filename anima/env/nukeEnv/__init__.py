289684be0e00d2d38e5969174a82087fdcb94a3d
Mehmet Erer <erer.mt@gmail.com>
2024-07-03 18:28:41 +0300

    * [Nuke] Default parameters updated in create_main_write_nodes().

--- anima/env/nukeEnv/__init__.py
+++ anima/env/nukeEnv/__init__.py
@@ -316,6 +316,8 @@ class Nuke(EnvironmentBase):
                     main_write_node["colorspace"].setValue(6)  # ACES 2065-1
                 main_write_node["datatype"].setValue(0)     # 16 bit half
                 main_write_node["compression"].setValue(1)  # Zip (1 scanline)
+                main_write_node["metadata"].setValue(4)  # all metadata
+                main_write_node["autocrop"].setValue(1)  # autocrop
                 # TODO: Tis is not generic. Fix ASAP.
                 if version.task.project.name in ['Helgoland', 'Kein Tier', 'Wolf Hall']:
                     main_write_node["compression"].setValue(3)  # PIZ Wavelet
