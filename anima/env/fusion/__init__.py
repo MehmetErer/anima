c5af19f4447e60da8b70f330c3f7f7a04d9b9b17
Mehmet Erer <erer.mt@gmail.com>
2024-11-01 14:17:12 +0300

    * [Fusion] Projects updated for output specs under create_main_saver_node().

--- anima/env/fusion/__init__.py
+++ anima/env/fusion/__init__.py
@@ -1720,7 +1720,7 @@ class Fusion(EnvironmentBase):
             })
 
         # TODO: This is not generic. Fix ASAP.
-        if version.task.project.name in ['Helgoland', 'Kein Tier', 'Wolf Hall', 'Castle']:
+        if version.task.project.name in ['Helgoland', 'Kein Tier', 'Wolf Hall', 'Castle', 'Drowak']:
             try:
                 output_format_data[1]['node_tree']['input_list']['OpenEXRFormat.Compression'] = 4
                 output_format_data[1]['node_tree']['input_list']['OpenEXRFormat.AlphaEnable'] = 1
