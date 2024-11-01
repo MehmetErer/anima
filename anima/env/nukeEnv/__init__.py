d0f8719fbdbde86e9b4ded1e4f6f4e60c95bc786
Mehmet Erer <erer.mt@gmail.com>
2024-11-01 14:17:32 +0300

    * [Nuke] Projects updated for output specs under create_main_write_nodes().

--- anima/env/nukeEnv/__init__.py
+++ anima/env/nukeEnv/__init__.py
@@ -319,7 +319,7 @@ class Nuke(EnvironmentBase):
                 main_write_node["metadata"].setValue(4)  # all metadata
                 main_write_node["autocrop"].setValue(1)  # autocrop
                 # TODO: Tis is not generic. Fix ASAP.
-                if version.task.project.name in ['Helgoland', 'Kein Tier', 'Wolf Hall', 'Castle']:
+                if version.task.project.name in ['Helgoland', 'Kein Tier', 'Wolf Hall', 'Castle', 'Drowak']:
                     main_write_node["compression"].setValue(3)  # PIZ Wavelet
                     main_write_node["metadata"].setValue(4)  # all metadata
             elif output_format_enum == 'mov':
